27a71fc4591a1a55118c029d9371d5b8025127aa
Zhe Feng <zhe.feng@fixposition.com>
2023-04-14 14:59:19 +0200

    fix the similar problem as ROS1

--- fixposition_driver_ros2/src/fixposition_driver_node.cpp
+++ fixposition_driver_ros2/src/fixposition_driver_node.cpp
@@ -70,6 +70,8 @@ void FixpositionDriverNode::Run() {
 
             rclcpp::sleep_for(reconnect_delay);
             Connect();
+        } else {
+            rate.sleep();
         }
     }
 }
@@ -113,16 +115,18 @@ void FixpositionDriverNode::RegisterObservers() {
                     }
 
                     // TFs
-                    geometry_msgs::msg::TransformStamped tf_ecef_poi;
-                    geometry_msgs::msg::TransformStamped tf_ecef_enu;
-                    geometry_msgs::msg::TransformStamped tf_ecef_enu0;
-                    TfDataToMsg(data.tf_ecef_poi, tf_ecef_poi);
-                    TfDataToMsg(data.tf_ecef_enu, tf_ecef_enu);
-                    TfDataToMsg(data.tf_ecef_enu0, tf_ecef_enu0);
+                    if (data.vrtk.fusion_status > 0) {
+                        geometry_msgs::msg::TransformStamped tf_ecef_poi;
+                        geometry_msgs::msg::TransformStamped tf_ecef_enu;
+                        geometry_msgs::msg::TransformStamped tf_ecef_enu0;
+                        TfDataToMsg(data.tf_ecef_poi, tf_ecef_poi);
+                        TfDataToMsg(data.tf_ecef_enu, tf_ecef_enu);
+                        TfDataToMsg(data.tf_ecef_enu0, tf_ecef_enu0);
 
-                    br_->sendTransform(tf_ecef_enu);
-                    br_->sendTransform(tf_ecef_poi);
-                    static_br_->sendTransform(tf_ecef_enu0);
+                        br_->sendTransform(tf_ecef_enu);
+                        br_->sendTransform(tf_ecef_poi);
+                        static_br_->sendTransform(tf_ecef_enu0);
+                    }
                 });
         } else if (format == "LLH" && converters_["LLH"]) {
             dynamic_cast<LlhConverter*>(converters_["LLH"].get())->AddObserver([this](const NavSatFixData& data) {
