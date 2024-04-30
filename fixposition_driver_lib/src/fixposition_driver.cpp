d6dd4ff040d5d24ce595ed9d48ed1d6d864e957f
Giuseppe Rizzi <giuseppe.rizzi@ascento.ch>
2024-04-30 15:37:26 +0200

    debug print

--- fixposition_driver_lib/src/fixposition_driver.cpp
+++ fixposition_driver_lib/src/fixposition_driver.cpp
@@ -287,6 +287,7 @@ bool FixpositionDriver::ReadAndPublish() {
 
 void FixpositionDriver::NmeaConvertAndPublish(const std::string& msg) {
     // split the msg into tokens, removing the *XX checksum
+    std::cout << "Converting msg: " << msg << std::endl;
     std::vector<std::string> tokens;
     std::size_t star_pos = msg.find_last_of("*");
     SplitMessage(tokens, msg.substr(1, star_pos - 1), ",");
@@ -311,9 +312,7 @@ void FixpositionDriver::NmeaConvertAndPublish(const std::string& msg) {
 
     // If we have a converter available, convert to ros.
     // Currently supported are "FP", "LLH", "ODOMETRY", "ODOMSH", "TF", "RAWIMU", "CORRIMU"
-    std::cout << "Got message: " << header << "\n";
     if (a_converters_[header] != nullptr) {
-        std::cout << "Converting message \n";
         a_converters_[header]->ConvertTokens(tokens);
     }
 }
