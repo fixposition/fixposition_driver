407fbc8bf9b8aedce8c301779bffae071a19c44f
fixposition-support <github@fixposition.com>
2024-08-28 17:11:56 +0200

    Fixed invalid TF check (#66)
    * Fixed invalid TF check
    
    * Improved syntaxis
    
    ---------
    
    Co-authored-by: Facundo Garcia <facundo.garcia@fixposition.com>

--- fixposition_driver_lib/src/messages/fpa/tf.cpp
+++ fixposition_driver_lib/src/messages/fpa/tf.cpp
@@ -68,7 +68,7 @@ void FP_TF::ConvertFromTokens(const std::vector<std::string>& tokens) {
                                  tokens.at(orientation_y_idx), tokens.at(orientation_z_idx));
 
     // Check if TF is valid
-    if (!tf.rotation.vec().isZero() && !(tf.rotation.w() == 0)) valid_tf = true;
+    valid_tf = !(tf.rotation.w() == 0 && tf.rotation.vec().isZero());
 }
 
 }  // namespace fixposition
