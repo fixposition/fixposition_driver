51bb923b0820f94478ac44887e5d56ce7ef8cee0
Giuseppe Rizzi <giuseppe.rizzi@ascento.ch>
2025-10-22 10:03:05 +0200

    apply path from @flipflip

--- fixposition_driver_ros1/include/fixposition_driver_ros1/ros1_msgs.hpp
+++ fixposition_driver_ros1/include/fixposition_driver_ros1/ros1_msgs.hpp
@@ -55,8 +55,8 @@
 #include <fixposition_driver_msgs/NmeaVtg.h>
 #include <fixposition_driver_msgs/NmeaZda.h>
 // - NOV-B
-#include <fixposition_driver_msgs/NovbInspvax.h>
 #include <fixposition_driver_msgs/NovbHeading2.h>
+#include <fixposition_driver_msgs/NovbInspvax.h>
 
 #pragma GCC diagnostic pop
 #endif  // __FIXPOSITION_DRIVER_ROS1_ROS_MSGS_HPP__
