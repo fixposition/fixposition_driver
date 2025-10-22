5a3bf1a56ba4965b4a8f4cf126e449ff9a2820f7
Giuseppe Rizzi <giuseppe.rizzi@ascento.ch>
2025-10-22 11:21:25 +0200

    edits from suggestions (with pre-commit now)

--- fixposition_driver_ros2/src/data_to_ros2.cpp
+++ fixposition_driver_ros2/src/data_to_ros2.cpp
@@ -533,8 +533,10 @@ static void NovbHeading2ToMsg(const novb::NovbHeader* header, const novb::NovbHe
         msg.reserved = payload->reserved;
         msg.heading_stdev = payload->heading_stdev;
         msg.pitch_stdev = payload->pitch_stdev;
-        std::copy(std::begin(payload->rover_stn_id), std::end(payload->rover_stn_id), std::begin(msg.rover_stn_id));
-        std::copy(std::begin(payload->master_stn_id), std::end(payload->master_stn_id), std::begin(msg.master_stn_id));
+        msg.rover_stn_id = fpsdk::common::string::BufToStr(
+            {payload->rover_stn_id, payload->rover_stn_id + sizeof(payload->rover_stn_id)});
+        msg.master_stn_id = fpsdk::common::string::BufToStr(
+            {payload->master_stn_id, payload->master_stn_id + sizeof(payload->master_stn_id)});
         msg.num_sv_tracked = payload->num_sv_tracked;
         msg.num_sv_in_sol = payload->num_sv_in_sol;
         msg.num_sv_obs = payload->num_sv_obs;
